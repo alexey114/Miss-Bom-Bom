6a35cf15430be7300a2f185f0c4dfb224c124a5e
alexey114 <bomkoar@gmail.com>
2021-03-11 00:28:05 +0300

    add js files for range.html

---
+++ js/catalog.js
@@ -0,0 +1,48 @@
+const CATALOG = [ // Создаем массив состоящих из объектов
+    {
+        id: 'el1',
+        name: 'Сережки-1',
+        img: '../img/hand_1.png',
+        price: 100,
+    }
+
+    {
+        id: 'el2',
+        name: 'Сережки-2',
+        img: '../img/hand_1.png',
+        price: 200,
+    }
+
+    {
+        id: 'el3',
+        name: 'Сережки-3',
+        img: '../img/hand_1.png',
+        price: 300,
+    }
+
+    {
+        id: 'el4',
+        name: 'Сережки-4',
+        img: '../img/hand_1.png',
+        price: 400,
+    }
+
+    {
+        id: 'el5',
+        name: 'Сережки-5',
+        img: '../img/hand_1.png',
+        price: 500,
+    }
+
+    {
+        id: 'el6',
+        name: 'Сережки-6',
+        img: '../img/hand_1.png',
+        price: 600,
+    }
+
+];
+
+// imgbb.com - сервис для сохранения фото, так же сохранять фото можно в Git Hub
+
+//https://www.youtube.com/watch?v=aI2ZjquntDQ&list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw&index=5
