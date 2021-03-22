30ade25ca3ec6d29beca2c42a376e7ade74ee8fc
alexey114 <bomkoar@gmail.com>
2021-03-22 13:30:10 +0300

    added li and ul in products+render

--- js/products.js
+++ js/products.js
@@ -1,11 +1,30 @@
 class Products {
     render() { //Метод render отображает даные на странице
-        CATALOG.forEach(({ id, name, price, img}) => { //Перебираем элементы из catalog.js + деструктуризация через {}
-            console.log(id, name, price, img); //В итоге вывод будет отдельными переменными
+        let htmlCatalog = '';
+
+        CATALOG.forEach(({id,name,price,img}) => { //Перебираем элементы из catalog.js + деструктуризация через {}
+            //console.log(id, name, price, img); //В итоге вывод будет отдельными переменными
             // console.log(element.id); //Получение доступа через точку
-        })
-    }
-}
+
+            htmlCatalog += ` 
+                <li>
+                    <span>${name}</span>
+                    <img src="${img}" />
+                    <span>${price}</span>
+                    <button>Добавить в корзину</button>
+                </li>
+            `; //Добавление элементов li в HTML
+        });
+
+        const html = `
+            <ul>
+                ${htmlCatalog}
+            </ul>        
+        `; //Добавление элементов ul в HTML
+        
+        ROOT_PRODUCTS.innerHTML = html;
+    };
+};
 
 const productsPage = new Products(); //создание экземпляра
 productsPage.render();//вызов метода render
