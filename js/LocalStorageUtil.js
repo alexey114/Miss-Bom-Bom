a1800c07ed6dc159234d6578803ea6f6c61c6b4d
alexey114 <bomkoar@gmail.com>
2021-03-26 14:34:21 +0300

    bug fix push LocalStorage

--- js/LocalStorageUtil.js
+++ js/LocalStorageUtil.js
@@ -4,26 +4,25 @@ class LocalStorageUtil {
     }
 
     getProducts () {
-        const productsLocalStorage = localStorage.getItem (this.keyName); //если будет значение верется строка, иначе вернется null
+        const productsLocalStorage = localStorage.getItem (this.keyName); //если будет значение вернется строка, иначе вернется null
         if (productsLocalStorage !== null) {
             return  JSON.parse(productsLocalStorage);
         }
         return []; //в противном случае получаем пустой массив
     } //метод позволяющий получить данные из локального хранилища
 
-    putProducts (id) { 
+    putProducts(id){ 
         let products = this.getProducts(); //берем всё, что находится в локальном хранилище
         let pushProduct = false; //Для выделения кнопки при удалении товара и добавления определяем переменную
-        const index = products.indexOff(id); //что бы не добавлялись дубли индексов, а удалялись делаем проверку
+        const index = products.indexOf(id); //что бы не добавлялись дубли индексов, а удалялись делаем проверку
 
             if(index===-1){ //если равен -1 (отсутствует), то нужнол записать
-                products.push(id);
+                products.push(id); //закидываем новый id
                 pushProduct = true;
             } else {
                 products.splice(index, 1); //в ином случае удалить
             }
 
-        products.push(id); //закидываем новый id
         localStorage.setItem(this.keyName, JSON.stringify(products)); //Устанавливаем в локальное хранилище
 
         return {
