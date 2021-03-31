94ccd8990a7c7d642168e487dc22cc08ec8a3ac8
alexey114 <bomkoar@gmail.com>
2021-03-31 12:55:28 +0300

    rename header in header-basket

--- js/root.js
+++ js/root.js
@@ -1,3 +1,3 @@
 const ROOT_PRODUCTS = document.getElementById('products'); //вызов products
-const ROOT_HEADER = document.getElementById('header');
+const ROOT_HEADER = document.getElementById('header-basket');
 const ROOT_SHOPPING = document.getElementById('shopping');
